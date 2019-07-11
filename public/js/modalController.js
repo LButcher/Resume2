b7566c74bbb8fddb3f7524ef4eb479c2cc7ad907
LButcher <liam.butcher@hotmail.com>
2019-07-11 19:10:17 -0400

    Started modal work and fixed mobile flexbox

---
+++ public/js/modalController.js
@@ -0,0 +1,32 @@
+let modal = null;
+
+function gridButtonPressed(element) {
+    try {
+        let btn = element.id;
+        console.log(element);
+        setModal(btn);
+    }
+    catch(e){
+        console.log(e);
+    }
+}
+
+function setModal(item){
+    document.querySelector('#mainBody').style.overflow='hidden';
+    let modalName = '#modal_'+item;
+    modal = document.querySelector(modalName);
+    modal.style.display = "block";
+    modal.focus();
+    modal.innerHTML = '<object type="text/html" data="../views/cpsc319Modal.html"></object>'
+}
+
+function closeModal(){
+    document.querySelector('#mainBody').style.overflow='auto';
+    modal.style.display = "none";
+}
+
+document.addEventListener("keydown", event => {
+    if(event.key=='Escape'){
+        closeModal();
+    }
+})
