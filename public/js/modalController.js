1d35d725b74f51ad08d2accd4fc03880aa9ef523
LButcher <liam.butcher@hotmail.com>
2019-07-22 16:22:27 -0400

    Some styling bug fixes and initial modal template ideas

--- public/js/modalController.js
+++ public/js/modalController.js
@@ -25,25 +25,23 @@ function openModal(btnName) {
     const scrollY = document.documentElement.style.getPropertyValue('--scroll-y');
     document.querySelector('body').style.position = 'auto';
     document.querySelector('body').style.overflowY = 'scroll';
-    const body = document.querySelector('html');
-    body.style.position = 'fixed';
-    body.style.top = `-${scrollY}`;
-
+    const doc = document.querySelector('html');
+    doc.style.position = 'fixed';
+    doc.style.top = `-${scrollY}`;
     modal = document.querySelector('#main-modal');
     modal.style.display = "block";
     modal.focus();
     let modalTemplate = getTemplate(btnName);
     modal.innerHTML = modalTemplate.innerHTML;
-
 }
 
 function closeModal() {
     //document.querySelector('#mainBody').style.overflow = 'auto';
     //const body = document.body;
-    const body = document.querySelector('html');
-    const scrollY = body.style.top;
-    body.style.position = '';
-    body.style.top = '';
+    const doc = document.querySelector('html');
+    const scrollY = doc.style.top;
+    doc.style.position = '';
+    doc.style.top = '';
     document.querySelector('html').style.scrollBehavior = 'auto';
     window.scrollTo(0, parseInt(scrollY || '0') * -1);
     document.querySelector('html').style.scrollBehavior = 'smooth';
