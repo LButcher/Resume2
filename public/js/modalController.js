2bd712229fe81b8db4126829b2affd30a63ed0e8
LButcher <liam.butcher@hotmail.com>
2019-07-27 17:54:01 -0400

    Changed font to Roboto and added nav bar tracker

--- public/js/modalController.js
+++ public/js/modalController.js
@@ -54,14 +54,38 @@ function getTemplate(id) {
 }
 
 function setNav() {
+
+    const topMargin = 100; //20px from top of window innerheight
+    const nav = document.querySelector('.main-nav');
     const titleY = document.querySelector('.heading-title').getBoundingClientRect().top;
     const scrollY = document.documentElement.style.getPropertyValue('--scroll-y');
-    const nav = document.querySelector('.main-nav');
-    if (parseInt(scrollY) < 800 && titleY > 20) {
+    const aboutY = document.querySelector('#about').getBoundingClientRect().top;
+    const projectsY = document.querySelector('#projects').getBoundingClientRect().top;
+    //const contactY = document.querySelector('#contact').getBoundingClientRect().top;
+    if (inTitle(scrollY, titleY)) {
         nav.style.backgroundColor = 'transparent';
-    } else if (parseInt(scrollY) >= 800 || titleY <= 20) {
+        document.querySelector('#about-option').style.textDecoration = 'none';
+        document.querySelector('#projects-option').style.textDecoration = 'none';
+        document.querySelector('#contact-option').style.textDecoration = 'none';
+
+    } else {
         nav.style.backgroundColor = 'black';
     }
+    if (aboutY <= topMargin && aboutY > 0) {
+        document.querySelector('#about-option').style.textDecoration = 'underline';
+        document.querySelector('#projects-option').style.textDecoration = 'none';
+        document.querySelector('#contact-option').style.textDecoration = 'none';
+
+    } else if (projectsY <= topMargin && projectsY > 0) {
+        document.querySelector('#about-option').style.textDecoration = 'none';
+        document.querySelector('#projects-option').style.textDecoration = 'underline';
+        document.querySelector('#contact-option').style.textDecoration = 'none';
+    }
+
+}
+
+function inTitle(scrollY, titleY) {
+    return (parseInt(scrollY) < 800 && titleY > 20);
 }
 
 document.addEventListener('keydown', event => {
