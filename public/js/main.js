c9e53e443d8061d336b78fec221defe9f07a2f12
LButcher <liam.butcher@hotmail.com>
2019-08-10 20:19:45 -0400

    Added about animations and some small layout changes

--- public/js/main.js
+++ public/js/main.js
@@ -5,6 +5,20 @@ let cards = [
     { name: 'robotsCard', shown: false }
 ];
 
+let aboutItems = [
+    { name: 'about-item-1', shown: false },
+    { name: 'about-item-2', shown: false },
+    { name: 'about-item-3', shown: false },
+
+]
+
+function checkBrowser() {
+    var ua = window.navigator.userAgent.indexOf("Trident");
+    if (ua >= 0) {
+        alert("I currently do not support Internet Explorer. Main functionality should still work but may be displayed incorrectly. Please use any other modern browser. Thanks!")
+    }
+}
+
 // Triggered when the "Read More" button is pressed in the projects grid
 function gridButtonPressed(element) {
     try {
@@ -22,12 +36,28 @@ function gridButtonPressed(element) {
 // Scrolls to set section in page. 
 // targetElement will be About, Projects or Contact
 function pageScroll(e, targetElement) {
+    if (e.srcElement.offsetParent.className == "main-nav") {
+        document.querySelector('#navToggle').checked = false;
+    }
     e.preventDefault();
     const targetElementId = '#' + targetElement;
     const elementY = document.querySelector(targetElementId).getBoundingClientRect().top;
     const navHeight = document.querySelector('nav').getBoundingClientRect().height;
     const scrollToY = parseInt(elementY) - navHeight;
     window.scrollBy(0, scrollToY);
+}
+
+function checkAboutItems() {
+    //Traditional for loop for IE11....
+    for (let i = 0; i < aboutItems.length; i++) {
+        let currItem = document.querySelector('#' + aboutItems[i].name);
+        let currItemPos = currItem.getBoundingClientRect();
+        if (currItemPos.top <= window.innerHeight && !aboutItems[i].shown) {
+            aboutItems[i].shown = true;
+            addRule(aboutItems[i].name, currItem, currItemPos);
+            aboutItems.splice(i, 1);
+        }
+    }
 }
 
 function checkCards() {
@@ -38,17 +68,18 @@ function checkCards() {
         if (currCardPos.top <= window.innerHeight && !cards[i].shown) {
             cards[i].shown = true;
             addRule(cards[i].name, currCard, currCardPos);
+            cards.splice(i, 1);
         }
     }
 
 }
 
-function addRule(cardName, currCard, currCardPos) {
-    let left = currCardPos.x;
+function addRule(elementName, currElement, currPos) {
+    let left = currPos.x;
     let keyFrame = "0%{transform:translateX(-" + left + "px);}";
-    document.styleSheets[0].insertRule("@keyframes " + cardName + "-entry {" + keyFrame + "100%{transform:translateX(0);}}", 0);
-    currCard.style.opacity = 1;
-    currCard.style.animation = cardName + "-entry 500ms";
+    document.styleSheets[0].insertRule("@keyframes " + elementName + "-entry {" + keyFrame + "100%{transform:translateX(0);}}", 0);
+    currElement.style.opacity = 1;
+    currElement.style.animation = elementName + "-entry 500ms";
 }
 
 document.addEventListener('keydown', function(event) {
@@ -61,5 +92,10 @@ document.addEventListener('keydown', function(event) {
 window.addEventListener('scroll', function() {
     document.documentElement.style.setProperty("--scrolltrack", window.pageYOffset + "px");
     setNav();
-    checkCards();
+    if (cards.length > 0) {
+        checkCards();
+    }
+    if (aboutItems.length > 0) {
+        checkAboutItems();
+    }
 });
